817c136349c11908128e9881125da94eef61ee3d
iqbalya25 <iqbalya25@gmail.com>
2025-06-05 19:08:31 +0700

    fixing page.tsx error for deploy

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+const about = () => {
+  return <div>page</div>;
+};
+
+export default about;
